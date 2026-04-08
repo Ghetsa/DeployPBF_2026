af418dbae5b0d540d879fa95aab9ce548198beab
Ghetsa Ramadhani <ghetsa.arryanti@gmail.com>
2026-04-08 18:27:40 +0700

    fix: SSR Detail Produk

--- my-app/src/pages/produk/[produk].tsx
+++ my-app/src/pages/produk/[produk].tsx
@@ -6,41 +6,28 @@ import useSWR from "swr"
 import DetailProduk from "../../views/DetailProduct"
 import { ProductType } from "@/types/Product.type"
 
-const HalamanProduk = ({ product }: { product: ProductType }) => {
+const HalamanProduk = ({product}:{product: ProductType}) => {
+//digunakan client-side rendering
+// // const Router = useRouter(); ...
 
-  return (
-    <div>
-      <DetailProduk products={product} />
-    </div>
-  )
+return (
+  <div>
+    <DetailProduk products={product} />
+  </div>
+ )
 }
 
 export default HalamanProduk
 
 //digunakan server-side rendering
 export async function getServerSideProps({ params }: { params: { produk: string } }) {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/produk/${params?.produk}`);
-
-    if (!res.ok) {
-      return { notFound: true };
-    }
-
-    const response = await res.json();
-
-    if (!response.data) {
-      return { notFound: true };
-    }
-
-    return {
-      props: {
-        product: response.data,
-      },
-    };
-  } catch (error) {
-    console.error("ERROR DETAIL:", error);
-
-    return { notFound: true };
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/produk/${params?.produk}`);
+  const respone = await res.json();
+  // console.log("Data produk yang diambil dari API:", respone);
+  return {
+    props: {
+      product: respone.data, // Pastikan untuk memberikan nilai default jika data tidak tersedia
+    },
   }
 }
 
